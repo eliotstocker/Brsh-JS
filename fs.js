fc9934a49f1966384e814cfe0e3e0ccca9db4503
Eliot Stocker <eliot.stocker@gmail.com>
2019-10-12 16:22:54 +0100

    chore: add demo to github pages

---
+++ fs.js
@@ -0,0 +1,11 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.filesystem = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports='#!/sh.js\nexport NAME="BRowserSHell"\nalias hi=\'echo "my name is $NAME"\'\n\ncat /root/motd';
+
+},{}],2:[function(require,module,exports){
+module.exports="Hello Welcome to a BRowserSHell Demo Environment, its kinda like sh on unix";
+
+},{}],3:[function(require,module,exports){
+module.exports={root:{".profile":require(1),motd:require(2)}};
+
+},{"1":1,"2":2}]},{},[3])(3)
+});
